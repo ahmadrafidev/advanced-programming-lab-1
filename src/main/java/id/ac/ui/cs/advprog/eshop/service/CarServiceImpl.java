555f3eb52c362e349e775579d0ff1563822a930c
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2024-02-18 12:38:41 +0700

    feat: implement OCP on  car repository

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
@@ -4,7 +4,6 @@ import id.ac.ui.cs.advprog.eshop.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList; 
-import java.util.Iterator;
 import java.util.List;
 
 @Service
@@ -14,34 +13,26 @@ public class CarServiceImpl implements CarService {
 
     @Override
     public Car create(Car car) {
-        // TODO Auto-generated method stub
-        carRepository.create(car);
-        return car;
+        return carRepository.create(car);
     }
 
     @Override
     public List<Car> findAll() {
-        Iterator<Car> carIterator = carRepository.findAll();
-        List<Car> allCar = new ArrayList<>();
-        carIterator.forEachRemaining (allCar::add);
-        return allCar;
+        return new ArrayList<>(carRepository.findAll());
     }
 
     @Override
     public Car findById(String carId){
-        Car car = carRepository.findById(carId);
-        return car;
+        return carRepository.findById(carId);
     }
 
     @Override
     public void update(String carId, Car car) {
-        // TODO Auto-generated method stub
         carRepository.update(carId, car);
     }
 
     @Override
     public void deleteCarById(String carId) {
-    // TODO Auto-generated method stub
         carRepository.delete(carId);
     }
 }
