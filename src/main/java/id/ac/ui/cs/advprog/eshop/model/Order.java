c553db3f5e13ae6ebec8448ef206e605ca621665
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2024-02-27 00:00:48 +0700

    [REFACTOR] Apply OrderStatus enum check to Order model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -1,9 +1,8 @@
 package id.ac.ui.cs.advprog.eshop.model;
 import lombok.Builder;
 import lombok.Getter;
-
-import java.util.Arrays;
 import java.util.List;
+import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
 
 @Getter
 @Builder
@@ -28,23 +27,14 @@ public class Order {
     
     public Order(String id, List<Product> products, Long orderTime, String author, String status) {
         this(id, products, orderTime, author); 
-
-        String[] statusList = {"WAITING_PAYMENT", "FAILED", "SUCCESS", "CANCELLED"};
-        if (Arrays.stream(statusList).noneMatch(item -> item.equals(status))) {
-            throw new IllegalArgumentException();
-        } else {
-            this.status = status;
-        }
-        
+        this.setStatus(status);
     }
 
     public void setStatus(String status) {
-        String[] statusList = {"WAITING_PAYMENT", "FAILED", "SUCCESS", "CANCELLED"};
-        
-        if (Arrays.stream(statusList).noneMatch(item -> item.equals(status))) {
-            throw new IllegalArgumentException();
-        } else {
+        if (OrderStatus.contains(status)) {
             this.status = status;
+        } else {
+            throw new IllegalArgumentException();
         }
     }
 }
--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -13,11 +13,11 @@ class OrderTest {
     void setUp() {
         this.products = new ArrayList<>();
         Product product1 = new Product();
-        product1.setProductId("e5b5589e-f1c9-460e-8860-71af6af63bd6");
+        product1.setProductId(12);
         product1.setProductName("Sampo Cap Bambang");
         product1.setProductQuantity(2);
         Product product2 = new Product();
-        product2.setProductId("a2c62328-4a37-4664-83c7-f32db8620155");
+        product2.setProductId(22);
         product2.setProductName("Sabun Cap Usep");
         product2.setProductQuantity(1);
         this.products.add(product1);
@@ -28,24 +28,17 @@ class OrderTest {
     void testCreateOrderEmptyProduct() {
         this.products.clear();
         assertThrows(IllegalArgumentException.class, () -> {
-            Order order = new Order(id: "13652556-012a-4c07-b546-54eb1396d79b", 
-                                    this.products, 
-                                    orderTime: 1708560000L, 
-                                    author: "Safira Sudrajat");
+            Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b", this.products, 1708560000L, "Safira Sudrajat");
         });
     }
 
     @Test
     void testCreateOrderDefaultStatus() {
-        Order order = new Order(id: "13652556-012a-4c07-b546-54eb1396d79b", 
-                                this.products, 
-                                orderTime: 1708560000L, 
-                                author: "Safira Sudrajat");
+        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b", this.products, 1708560000L, "Safira Sudrajat");
         assertSame(this.products, order.getProducts());
         assertEquals(2, order.getProducts().size());
         assertEquals("Sampo Cap Bambang", order.getProducts().get(0).getProductName());
-        assertEquals("Sabun Cap Usep", order.getProducts().get(1).getProductName());
-        
+        assertEquals("Sabun Cap Usep", order.getProducts().get(1).getProductName());  
         assertEquals("13652556-012a-4c07-b546-54eb1396d79b", order.getId());
         assertEquals(1708560000L, order.getOrderTime());
         assertEquals("Safira Sudrajat", order.getAuthor());
@@ -54,41 +47,27 @@ class OrderTest {
 
     @Test
     void testCreateOrderSuccessStatus() {
-        Order order = new Order(id: "13652556-012a-4c07-b546-54eb1396d79b", 
-                                this.products, 
-                                orderTime: 1708560000L, 
-                                author: "Safira Sudrajat", 
-                                status: "SUCCESS");
+        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b", this.products, 1708560000L, "Safira Sudrajat", "SUCCESS");
         assertEquals("SUCCESS", order.getStatus());
     }
 
     @Test
     void testCreateOrderInvalidStatus() {
         assertThrows(IllegalArgumentException.class, () -> {
-            Order order = new Order(id: "13652556-012a-4c07-b546-54eb1396d79b", 
-                                    this.products, 
-                                    orderTime: 1708560000L, 
-                                    author: "Safira Sudrajat", 
-                                    status: "MEOW");
+            Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b", this.products, 1708560000L, "Safira Sudrajat", "MEOW");
         });
     }
 
     @Test
     void testSetStatusToCancelled() {
-        Order order = new Order(id: "13652556-012a-4c07-b546-54eb1396d79b", 
-                                this.products, 
-                                orderTime: 1708560000L, 
-                                author: "Safira Sudrajat");
+        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b", this.products, 1708560000L, "Safira Sudrajat");
         order.setStatus("CANCELLED");
         assertEquals("CANCELLED", order.getStatus());
     }
 
     @Test
     void testSetStatusToInvalidStatus() {
-        Order order = new Order(id: "13652556-012a-4c07-b546-54eb1396d79b", 
-                                this.products, 
-                                orderTime: 1708560000L, 
-                                author: "Safira Sudrajat");
+        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b", this.products, 1708560000L, "Safira Sudrajat");
         assertThrows(IllegalArgumentException.class, () -> order.setStatus("MEOW"));
     }
 }
