ab8a476363a4fd9842069b815645715ec4fbdee7
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2024-02-26 23:52:05 +0700

    [REFACTOR] Add OrderStatus enum

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -3,7 +3,6 @@ package id.ac.ui.cs.advprog.eshop.model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.ArrayList;
 import java.util.List;
 
