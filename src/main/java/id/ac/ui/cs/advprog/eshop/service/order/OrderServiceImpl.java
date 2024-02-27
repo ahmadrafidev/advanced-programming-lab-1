20cd19d5f864f4b3b80ffce485c5fcd37cd9c593
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2024-02-27 08:44:20 +0700

    [RED] Add OrderServiceImpl skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/order/OrderServiceImpl.java
@@ -0,0 +1,29 @@
+package id.ac.ui.cs.advprog.eshop.service.order;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.repository.order.OrderRepository;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Override
+    public Order createOrder(Order order) { return null; }
+
+    @Override
+    public Order updateStatus(String orderId, String status) { return null; }
+
+    @Override
+    public List<Order> findAllByAuthor(String author) { return null; }
+
+    @Override
+    public Order findById(String orderId) { return null; }
+}
+
