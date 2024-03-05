7c071e74577ccd6c5f5c410fe39cee404bf8080c
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2024-03-05 16:22:47 +0700

    [GREEN] Adding Payment Model

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -0,0 +1,52 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import java.util.Map;
+
+public class Payment {
+    private String id;
+    private String method;
+    private String status;
+    private Map<String, String> paymentData;
+
+    // Constructor
+    public Payment(String id, String method, String status, Map<String, String> paymentData) {
+        this.id = id;
+        this.method = method;
+        this.status = status;
+        this.paymentData = paymentData;
+    }
+
+    // Getters
+    public String getId() {
+        return id;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Map<String, String> getPaymentData() {
+        return paymentData;
+    }
+
+    // Setters
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setPaymentData(Map<String, String> paymentData) {
+        this.paymentData = paymentData;
+    }
+}
